6d08834ad39e0c631998967cfcda82a065232649
German Pinilla <gpinilladev@gmail.com>
2021-11-19 18:28:56 -0500

    Se agregan ultimos cambios al formato de impresion de la certificacion de incapacidad

---
+++ src/app/pages/incapacidad/certificado-incapacidad-externo/certificado-incapacidad-externo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-certificado-incapacidad-externo',
+  templateUrl: './certificado-incapacidad-externo.component.html',
+  styleUrls: ['./certificado-incapacidad-externo.component.scss']
+})
+export class CertificadoIncapacidadExternoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
