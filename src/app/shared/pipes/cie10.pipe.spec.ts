49847efeb8aad1e43130f84fcb217cf97262f07e
German Pinilla <gpinilladev@gmail.com>
2022-06-03 13:01:43 -0500

    Test

--- src/app/shared/pipes/cie10.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Cie10Pipe } from './cie10.pipe';
-
-describe('Cie10Pipe', () => {
-  it('create an instance', () => {
-    const pipe = new Cie10Pipe();
-    expect(pipe).toBeTruthy();
-  });
-});
